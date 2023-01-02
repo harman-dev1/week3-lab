91e843642d6a9738416839650b43a39cfd7020d0
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-02 16:51:00 +0500

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+string name;
+int rollno;
+cout <<"Enter Name: ";
+cin >> name;
+cout <<"Enter Roll No: ";
+cin >> rollno;
+cout<<"Your Name is:"<<name<<endl;
+cout<<"Your Roll No is:"<<rollno;
+} 
