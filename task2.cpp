80eb76e05483cb27b86337aecc49f60f580c9921
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-02 16:51:21 +0500

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+int dollar;
+int ruppes=200;
+int convertedvalue;
+cout<<"Enter Dollar:";
+cin>> dollar;
+convertedvalue=dollar*ruppes;
+cout<<"convertedvalue is "<<convertedvalue;
+}
