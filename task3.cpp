0bbba9bce09db95f67b23e37b58ff8bdd8ce16c9
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-02 16:51:35 +0500

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+float weights=0.45;
+float pounds;
+float kg;
+cout<<"Enter pounds:";
+cin>> pounds;
+kg=pounds/weights;
+cout<<"converted weight in kgs is :"<<kg;
+}
