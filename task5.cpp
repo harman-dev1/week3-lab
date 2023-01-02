ae1a1c5de647e4758f63eb36f3a4696708200a67
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-02 16:51:57 +0500

    Add files via upload

---
+++ task5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int current;
+int time;
+int charge;
+cout<<"Enter the charge:";
+cin>>charge;
+cout<<"Enter the time:";
+cin>>charge;
+current=charge/time;
+cout<<"current is:";
+cout<<current;
+}
