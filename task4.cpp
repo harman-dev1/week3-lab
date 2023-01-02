3260873dbaad25a798c583c99ef72fd6e5cc04eb
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-02 16:52:14 +0500

    Add files via upload

---
+++ task4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+int length;
+int width;
+int area;
+cout<<"Enter the length of rectangle:";
+cin>>length;
+cout<<"Enter the width of rectangle:";
+cin>>width;
+area=length*width;
+cout<<"area of rectangle is:"<<area;
+}
