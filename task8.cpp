e3c283dc519be1f373cb23d8bdb470b1a55fab1b
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-02 16:53:29 +0500

    Add files via upload

---
+++ task8.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+main(){
+int num1;
+int num2;
+int num3;
+int num4;
+int num5;
+int num6;
+int num7;
+int num8;
+int num9;
+int num10;
+int add;
+int sub;
+int mul;
+int div;
+int result;
+cout<<"Enter number 1:";
+cin>>num1;
+cout<<"Enter number 2:";
+cin>>num2;
+cout<<"Enter number 3:";
+cin>>num3;
+cout<<"Enter number 4:";
+cin>>num4;
+cout<<"Enter number 5:";
+cin>>num5;
+cout<<"Enter number 6:";
+cin>>num6;
+cout<<"Enter number 7:";
+cin>>num7;
+cout<<"Enter number 8:";
+cin>>num8;
+cout<<"Enter number 9:";
+cin>>num9;
+cout<<"Enter number 10:";
+cin>>num10;
+add=num1+num2+num8+num9;
+sub=num6+num5;
+div=num7/num3;
+mul=num4*num10;
+cout<<"add of 4 numbers are:"<<add;
+cout<<"sub of 2 numbers are:"<<sub;
+cout<<"div of 2 numbers are:"<<div;
+cout<<"mul of 2 numbers are:"<<mul;
+result=add+sub+div+mul;
+cout<<result
+
+}
